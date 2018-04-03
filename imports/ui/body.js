f3a1cf8f5560a4bfac67af0b38ed2ae47fa5fa0e
jigar wala <jigar.wala@somaiya.edu>
2018-04-04 00:19:49 +0530

    complete crud

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,26 +5,22 @@ import { Todos } from "../api/db";
 
 Template.heading.events({
     'click div > h1 > i.fa-plus':function(e){
-        $("#todoText").slideToggle(300)
+        $("#todoText").slideToggle(200)
     }
 })
-Template.AddTodo.helpers({
 
-}) 
 Template.AddTodo.events({
 
     'keypress #todoText':function(e){
         // "till the key pressed is enter"
-        if (event.which === 13) {
-            
+        if (event.which === 13) {            
             var todoText = $(e.target).val();
-            console.log(todoText)
             $(e.target).val("");
-            
             Todos.insert({task:todoText,done:false,createdOn:Date()})
         }
     }
 })
+
 Template.ListTodos.helpers({
 
     read() {
@@ -35,20 +31,34 @@ Template.ListTodos.helpers({
 
 Template.ListTodos.events({
 
-'click ul > li':function(e){
-
-$(e.target).toggleClass("completed")
-
-},
-'click ul li span':function(e){
-    $(e.target).parent().fadeOut(500, function () {
-        $(this).remove();
-    });
-    e.stopPropagation();
-}
+    'click li span.content':function(e){
+        var spanContent =$(e.target);
+        spanContent.toggleClass("completed")
+        var li=spanContent.parent()[0];
+        var todo=Todos.findOne({_id:li.id})
+        if(todo !== undefined)
+        Todos.update({_id:li.id},{$set:{done:!todo.done}});
+    }
 })
 
-Template.Todos.onCreated(function(){
-    console.log("ef")
+Template.Todos.events({
+    'click span.delete': function (e) {
+        var li = $(e.target).parent();
+        _id=li[0].id;
+        li.fadeOut(500, function () {
+            li.remove();
+            Todos.remove({_id});
+        });
+        
+        e.stopPropagation();
+    },
+    'click span.delete i': function (e) {
+        var li = $(e.target).parent().parent();
+        _id=li[0].id;
+        li.fadeOut(500, function () {
+            li.remove();
+            Todos.remove({_id});
+        });
+        e.stopPropagation();
+    }
 })
-
